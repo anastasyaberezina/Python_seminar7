416a15cc02b350088ec8d3635533b4bec98aa0f1
Anastasiya B <gavrilovaanastasya@yandex.ru>
2022-12-26 14:18:07 +0300

    add calc

--- Calc/input.py
+++ Calc/input.py
@@ -1,1 +1,11 @@
 # Ввод данных (сбор информации)
+
+import random
+
+number = 0
+type_number = input('Введите тип числа real или complex:')
+type_number1 = 'real'
+type_number2 = 'complex'
+number_sign = '+'
+
+number_rund = random.randint(0, 1)
